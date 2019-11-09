319a61dc4aed7bfb8a9df9e11ed776b479467a6e
Minhaj Uddin Khan <minhajuddinkhan75>
2019-11-09 21:10:26 +0500

    Fix join game integration tests

---
+++ tests/game/game_get_test.go
@@ -0,0 +1,82 @@
+package webrung_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var PORT = os.Getenv("PORT")
+var HOST = os.Getenv("HOST")
+var API_URL = fmt.Sprintf("http://%s:%s", HOST, PORT)
+
+type GameResponse struct {
+	GameID        string `json:"game_id"`
+	PlayersJoined int    `json:"players_joined"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token,omitempty"`
+}
+
+func GetAuthToken(username string) (string, error) {
+
+	jsonBody := []byte(fmt.Sprintf(`{"username": "%s"}`, username))
+	url := fmt.Sprintf("%s/api/v1/auth", API_URL)
+	r, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return "", err
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		return "", err
+	}
+
+	var lr LoginResponse
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&lr)
+	return lr.Token, err
+}
+
+func TestGame_ShouldGetByID(t *testing.T) {
+
+	token, err := GetAuthToken("North")
+	assert.Nil(t, err)
+
+	c := http.Client{}
+	url := fmt.Sprintf("%s/api/v1/games", API_URL)
+	r, err := http.NewRequest(http.MethodPost, url, nil)
+	assert.Nil(t, err)
+	r.Header.Set("token", token)
+	r.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.Do(r)
+	assert.NotNil(t, resp)
+	assert.Nil(t, err)
+
+	dec := json.NewDecoder(resp.Body)
+	gr := GameResponse{}
+	err = dec.Decode(&gr)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+
+	c = http.Client{}
+	resp, err = c.Get(fmt.Sprintf("%s/api/v1/games/%s", API_URL, gr.GameID))
+	assert.Nil(t, err)
+
+	dec = json.NewDecoder(resp.Body)
+	var getGameResponse GameResponse
+	err = dec.Decode(&getGameResponse)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+
+	assert.Nil(t, err)
+	assert.Equal(t, gr.GameID, getGameResponse.GameID)
+}
--- tests/game/game_start_test.go
+++ tests/game/game_start_test.go
@@ -10,51 +10,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func CreateGame(token string) (GameResponse, error) {
-
-	c := http.Client{}
-	url := fmt.Sprintf("%s/api/v1/games", API_URL)
-	r, err := http.NewRequest(http.MethodPost, url, nil)
-	r.Header.Set("token", token)
-	r.Header.Set("Content-Type", "application/json")
-
-	var gr GameResponse
-	resp, err := c.Do(r)
-
-	if err != nil {
-		return gr, err
-	}
-
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(&gr)
-
-	if gr.GameID == "" {
-		return gr, fmt.Errorf("game id is empty")
-	}
-	return gr, err
-}
-
-func JoinGame(token string, gameID string) error {
-	c := http.Client{}
-	url := fmt.Sprintf("%s/api/v1/games/%s/join", API_URL, gameID)
-	r, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return err
-	}
-	r.Header.Set("token", token)
-	r.Header.Set("Content-Type", "application/json")
-
-	resp, err := c.Do(r)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("join game request failed")
-	}
-
-	return nil
-}
-
 func StartGame(gameID, token string) ([]entities.Player, error, int) {
 
 	c := http.Client{}
